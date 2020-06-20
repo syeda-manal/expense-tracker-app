4d78f3a520d98ccd57949de140b98c421bf24a76
syeda-manal <>
2020-06-20 16:40:45 +0500

    experiment

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import Testing from '../Testing'
-
 
 const Header = () => {
     return (
         <div className = "container">
-            <Testing />
              <center>
         <h1>
            Expense Tracker 
